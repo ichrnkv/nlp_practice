a3ee55af4e20e82c8e9dad3dfa70440876fc85b1
Ilya Chernikov <ichrnkv@yandex.ru>
2020-05-12 18:11:00 +0300

    Task1 done

---
+++ self_attention/task1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def calc_logits(matrix):
+    """
+    Self-attention Logits
+    :param matrix: numpy matrix
+    :return:
+    """
+    return np.dot(matrix, np.transpose(matrix))
+
+
+if __name__ == '__main__':
+    Input = np.array([[1, 0], [0, 1], [1, 1], [0, 0]])
+    Logits = calc_logits(Input)
+    print(Logits)
