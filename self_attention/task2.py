5e67ba58d9224abe192051147269299b2080c293
Ilya Chernikov <ichrnkv@yandex.ru>
2020-05-12 18:40:08 +0300

    Task2 done

--- self_attention/task2.py
+++ self_attention/task2.py
@@ -1,1 +1,32 @@
-1
+import numpy as np
+
+
+def calc_logits(matrix):
+    """
+    Self-attention Logits
+    :param matrix: numpy matrix
+    :return:
+    """
+    return np.dot(matrix, np.transpose(matrix))
+
+
+def rows_softmax(matrix):
+    """
+    Softmax by rows
+    :param matrix: numpy matrix
+    :return:
+    """
+    att_scores = np.zeros((matrix.shape))
+    for i, row in enumerate(matrix):
+        for j, value in enumerate(row):
+            att_scores[i][j] = np.exp(value)/sum(np.exp(row))
+
+    return att_scores
+
+
+if __name__ == '__main__':
+    Input = np.array([[1, 0], [0, 1], [1, 1], [0, 0]])
+    Logits = calc_logits(Input)
+    AttScores = rows_softmax(Logits)
+    Result = np.dot(AttScores, Input)
+    print(Result)
